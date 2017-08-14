9609b602fbd3729abb167e44992c25762b00915d
Brian Futrell <bfutrel@gmail.com>
2017-08-14 08:18:04 -0400

    added variables to format name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var formattedName;
+ var formattedRole;
+
+ formattedName = HTMLheaderName.replace("%data%", "Brian Futrell");
+ formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+ $("#header").html().prepend(formattedName);
+ $("#header").html().prepend(formattedRole);
