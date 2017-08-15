3e3824e7a1c2a3f3ee1b03e31d2a60a512f05ef9
Brian Futrell <bfutrel@gmail.com>
2017-08-15 07:52:07 -0400

    working on getting name to appear without errors

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var formattedName;
- var formattedRole;
+var formattedName;
+var formattedRole;
 
- formattedName = HTMLheaderName.replace("%data%", "Brian Futrell");
- formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+formattedName = HTMLheaderName.replace("%data%", "Brian Futrell");
+formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 
- $("#header").html().prepend(formattedName);
- $("#header").html().prepend(formattedRole);
+$("#header").prepend(formattedName);
+//$("#header").append(formattedRole);
