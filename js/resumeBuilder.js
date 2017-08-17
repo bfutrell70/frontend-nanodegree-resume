8f15a9bc96f77dc998076f6c7a76f64bd02f0fa8
bfutrell70 <bfutrel@gmail.com>
2017-08-17 11:45:31 -0400

    added comment regarding adding role then name.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,5 +11,7 @@ var role = "Web Developer";
 formattedName = HTMLheaderName.replace("%data%", name);
 formattedRole = HTMLheaderRole.replace("%data%", role);
 
+// since the role and name need to be added before anything else prepend the role
+// first, then the name so that the name will appear first.
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
