b9557cf2165574e3e914c1c384cc0b22d16862e8
bfutrell70 <bfutrel@gmail.com>
2017-08-21 16:03:23 -0400

    Added image and welcome message but they don't appear on the index page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,29 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = new {
+    "name" : "Brian Futrell",
+    "role" : "Web Developer",
+    "contactInfo" : ["919-741-1959", "4201 Ferriday Court Raleigh, NC 27616", "bfutrel@gmail.com"],
+    "pictureUrl" : "/images/fry.jpg",
+    "welcomeMessage" : "Nice to meet you!",
+    "skills" : ["C#", "ASP.NET", "SQL Server"]
+};
+
 
 var formattedName;
 var formattedRole;
+var formattedImage;
+var formattedWelcomeMessage;
 
-var name = "Brian Futrell";
-var role = "Web Developer";
+//var name = "Brian Futrell";
+//var role = "Web Developer";
 
-formattedName = HTMLheaderName.replace("%data%", name);
-formattedRole = HTMLheaderRole.replace("%data%", role);
+formattedName = HTMLheaderName.replace("%data%", bio.name);
+formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+formattedImage = HTMLbioPic.replace("%data%", bio.pictureUrl);
+formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
 // since the role and name need to be added before anything else prepend the role
 // first, then the name so that the name will appear first.
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#header").prepend(formattedImage);
+$("#header").append(formattedWelcomeMessage);
